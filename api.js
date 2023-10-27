abf88691f2838f0f547826176fce76ed2d6ad52c
ziigzagz <iffan.h@ideabranch.co.th>
2023-10-27 15:13:23 +0700

    update bot

--- api.js
+++ api.js
@@ -16,20 +16,59 @@ const text = async (prompt) => {
 
 // Chat service
 const chat = async (prompt) => {
-  
-};
-async function test(prompt){
-    // const client = new DiscussServiceClient({ authClient: new GoogleAuth().fromAPIKey(API_KEY) });
-    const client = new TextServiceClient({ authClient: new GoogleAuth().fromAPIKey(API_KEY) });
-  return await client.generateText({
-    model: "models/text-bison-001",
-    prompt: { text: prompt }
+  const client = new DiscussServiceClient({ authClient: new GoogleAuth().fromAPIKey(API_KEY) });
+  return await client.generateMessage({
+    model: "models/chat-bison-001",
+    prompt: {
+      context: "You are a Community Manager who works with many of developer communities in Thailand and want to help developers succeed by using LINE APIs.",
+      examples: [
+        {
+          input: { content: "What APIs does LINE provide?" },
+          output: { content: "Messaging API, LIFF, LINE Login, LINE Beacon, LINE Notify, LINE Things, and LINE MINI App."},
+        },
+        {
+          input: { content: "What can I do with the Messaging API?" },
+          output: { content: "Build a chatbot to interact with users on LINE."},
+        },
+        {
+          input: { content: "What can I do with the LIFF?" },
+          output: { content: "Build a web app that run within LINE."},
+        },
+        {
+          input: { content: "How to get started with LINE APIs?" },
+          output: { content: "You can go to LINE Developers Thailand website at https://linedevth.line.me and explore APIs that fit with your use case."},
+        }
+      ],
+      messages: [{ content: prompt }]
+    }
   });
+};
+async function test(){
+    const client = new DiscussServiceClient({ authClient: new GoogleAuth().fromAPIKey(API_KEY) });
+    return await client.generateMessage({
+      model: "models/chat-bison-001",
+      prompt: {
+        context: "You are a Community Manager who works with many of developer communities in Thailand and want to help developers succeed by using LINE APIs.",
+        examples: [
+          {
+            input: { content: "What APIs does LINE provide?" },
+            output: { content: "Messaging API, LIFF, LINE Login, LINE Beacon, LINE Notify, LINE Things, and LINE MINI App."},
+          },
+          {
+            input: { content: "What can I do with the Messaging API?" },
+            output: { content: "Build a chatbot to interact with users on LINE."},
+          },
+          {
+            input: { content: "What can I do with the LIFF?" },
+            output: { content: "Build a web app that run within LINE."},
+          },
+          {
+            input: { content: "How to get started with LINE APIs?" },
+            output: { content: "You can go to LINE Developers Thailand website at https://linedevth.line.me and explore APIs that fit with your use case."},
+          }
+        ],
+        messages: [{ content: prompt }]
+      }
+    });
 }
-
-async function main() {
-  const result = await test("show me how to get started with LINE APIs");
-  console.log(JSON.stringify(result));
-}
-main();
-// module.exports = { text, chat };
+module.exports = { text, chat };
--- index.js
+++ index.js
@@ -1,3 +1,170 @@
-const {onRequest} = require("firebase-functions/v2/https");
-const axios = require("axios");
-const palm = require("./palm"); // import palm module
+const line = require("@line/bot-sdk");
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+
+// Import dependencies
+const { TextServiceClient, DiscussServiceClient } =
+  require("@google-ai/generativelanguage").v1beta2;
+const { GoogleAuth } = require("google-auth-library");
+
+// const port = process.env.PORT || 3000;
+const port = 4000;
+
+const env = dotenv.config().parsed;
+const config = {
+  channelAccessToken: env.ACCESS_TOKEN,
+  channelSecret: env.SECRET_TOKEN,
+};
+// Get PaLM API key from Environment Variable
+const API_KEY = "AIzaSyBGK6kM7Jrum1CKyDtl_rZhKRBio3fbU4U";
+
+// Text service
+const client = new line.Client(config);
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  try {
+    const events = req.body.events;
+    console.log(events)
+    return events.length > 0
+      ? Promise.all(events.map(handleEvent))
+      : res.status(200).send("OK");
+  } catch (error) {
+    res.status(500).end();
+  }
+});
+
+async function test(prompt) {
+  // const client = new DiscussServiceClient({ authClient: new GoogleAuth().fromAPIKey(API_KEY) });
+  const client_ai = new TextServiceClient({
+    authClient: new GoogleAuth().fromAPIKey(API_KEY),
+  });
+  return new Promise(async (resolve, reject) => {
+    try {
+        var res = await client_ai.generateText({
+            model: "models/text-bison-001",
+            prompt: { text: prompt },
+          });
+          resolve(res);
+    } catch (error) {
+        resolve([
+            {
+              "candidates": [
+                {
+                  "safetyRatings": [
+                    {
+                      "category": "HARM_CATEGORY_DEROGATORY",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_TOXICITY",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_VIOLENCE",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_SEXUAL",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_MEDICAL",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_DANGEROUS",
+                      "probability": "NEGLIGIBLE"
+                    }
+                  ],
+                  "output": "ไม่รองรับภาษาไทย"
+                }
+              ],
+              "filters": [],
+              "safetyFeedback": []
+            },
+            null,
+            null
+          ]);
+    }
+  });
+  
+ 
+}
+async function test_chat(prompt) {
+  // const client = new DiscussServiceClient({ authClient: new GoogleAuth().fromAPIKey(API_KEY) });
+  const client_ai = new TextServiceClient({
+    authClient: new GoogleAuth().fromAPIKey(API_KEY),
+  });
+  return new Promise(async (resolve, reject) => {
+    try {
+        var res = await client_ai.generateText({
+            model: "models/text-bison-001",
+            prompt: { text: prompt },
+          });
+          resolve(res);
+    } catch (error) {
+        resolve([
+            {
+              "candidates": [
+                {
+                  "safetyRatings": [
+                    {
+                      "category": "HARM_CATEGORY_DEROGATORY",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_TOXICITY",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_VIOLENCE",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_SEXUAL",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_MEDICAL",
+                      "probability": "NEGLIGIBLE"
+                    },
+                    {
+                      "category": "HARM_CATEGORY_DANGEROUS",
+                      "probability": "NEGLIGIBLE"
+                    }
+                  ],
+                  "output": "ไม่รองรับภาษาไทย"
+                }
+              ],
+              "filters": [],
+              "safetyFeedback": []
+            },
+            null,
+            null
+          ]);
+    }
+  });
+  
+ 
+}
+
+const handleEvent = async (event) => {
+  // repyr msg back
+  console.log("Event incoming")
+  if (event.type === "message" && event.message.type === "text") {
+    // var res = await google_map(event.message.text);
+    let prompt = event.message.text;
+    var res = await test_chat(prompt);
+    console.log("🚀 ~ file: index.js:88 ~ handleEvent ~ res:", JSON.stringify(res));
+    console.log(event.message.text, JSON.stringify(res));
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `${res[0]?.candidates[0]?.output}`,
+    });
+  }
+};
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
